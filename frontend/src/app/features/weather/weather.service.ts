262c96c237a0df42356b7720307a59d1e4d082dc
mqlstam <118191161+mqlstam@users.noreply.github.com>
2024-12-20 11:30:25 +0100

    Revert "Weer"

--- frontend/src/app/features/weather/weather.service.ts
+++ frontend/src/app/features/weather/weather.service.ts
@@ -1,20 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class WeatherService {
-  private backendUrl = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) {}
-
-  getCurrentWeather(location: string): Observable<any> {
-    return this.http.get(`${this.backendUrl}/current/${location}`);
-  }
-
-  getWeatherForecast(location: string): Observable<any> {
-    return this.http.get(`${this.backendUrl}/forecast/${location}`);
-  }
+  constructor() { }
 }
