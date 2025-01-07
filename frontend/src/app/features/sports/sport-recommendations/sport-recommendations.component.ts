8df835d2635b9111c6a460257b529efed6764e10
not101Xander <xander380@gmail.com>
2025-01-07 15:10:44 +0100

    add: suggested sports component

--- frontend/src/app/features/sports/sport-recommendations/sport-recommendations.component.ts
+++ frontend/src/app/features/sports/sport-recommendations/sport-recommendations.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RcmdService } from '../../../service/rcmd.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sport-recommendations',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sport-recommendations.component.html',
   styleUrl: './sport-recommendations.component.css'
 })
-export class SportRecommendationsComponent {
+export class SportRecommendationsComponent implements OnInit {
+  suggestedSports: any[] = [];
+
+  constructor(private rcmdService: RcmdService) { }
+
+  ngOnInit(): void {
+    console.log(`Sport recommendations console called`)
+    this.getSuggestedSports;
+  }
+
+  getSuggestedSports() {
+    this.rcmdService.listSuggestedSports();
+    this.rcmdService.suggestedSports$.subscribe(sports => {
+      this.suggestedSports = sports;
+    })
+  }
 
 }
