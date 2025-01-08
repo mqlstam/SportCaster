d22791d6d89c7e380e335f85651be67baf70071d
not101Xander <xander380@gmail.com>
2025-01-08 13:10:29 +0100

    sport rec. now includes current weather

--- frontend/src/app/features/sports/sport-recommendations/sport-recommendations.component.ts
+++ frontend/src/app/features/sports/sport-recommendations/sport-recommendations.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RcmdService } from '../../../service/rcmd.service';
 import { CommonModule } from '@angular/common';
+import { WeatherService } from '../../weather/weather.service';
 
 @Component({
   selector: 'app-sport-recommendations',
@@ -11,12 +12,17 @@ import { CommonModule } from '@angular/common';
 })
 export class SportRecommendationsComponent implements OnInit {
   suggestedSports: any[] = [];
+  weatherData: any;
+  errorMessage: string | null = null;
+  lat: any;
+  lon: any;
 
-  constructor(private rcmdService: RcmdService) { }
+  constructor(private rcmdService: RcmdService, private weatherService: WeatherService) { }
 
   ngOnInit(): void {
     console.log(`Sport recommendations console called`)
     this.getSuggestedSports();
+    this.fetchWeather();
   }
 
   getSuggestedSports() {
@@ -26,5 +32,50 @@ export class SportRecommendationsComponent implements OnInit {
       this.suggestedSports = sports;
     });
   }
+
+  async fetchWeather(): Promise<void> {
+    try {
+    await this.getUserLocation();
+    console.log(`lat: ${this.lat}`)
+    const location = `${this.lat},${this.lon}`;
+    this.weatherService.getWeather(location).subscribe(
+      (data) => {
+        this.weatherData = data;
+        console.log(`Fetched weater: ${this.weatherData}`)
+      },
+      () => {
+        this.errorMessage = 'Error fetching current weather.';
+      }
+    );
+  } catch (error) {
+    console.log(`Failed to fetch weather due to loc error`)
+  }
+  }
+
+  getUserLocation(): Promise<void> {
+    return new Promise((resolve, reject) => {
+
+  
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.lat = position.coords.latitude;
+          this.lon = position.coords.longitude;
+          console.log(`Position: ${position.coords.latitude}`)
+          resolve();
+        },
+        (error) => {
+          this.errorMessage = 'Unable to retrieve your location. Please allow location access.';
+          reject();
+        }
+      );
+    } else {
+      this.errorMessage = 'Geolocation is not supported by your browser.';
+      reject();
+    }
+      })
+  }
+
+  
 
 }
