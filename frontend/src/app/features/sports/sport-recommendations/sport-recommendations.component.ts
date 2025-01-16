cabf4af36609d72640700f1ff6551c3c2f301651
not101Xander <xander380@gmail.com>
2025-01-16 03:49:31 +0100

    added working icons for all available sports

--- frontend/src/app/features/sports/sport-recommendations/sport-recommendations.component.ts
+++ frontend/src/app/features/sports/sport-recommendations/sport-recommendations.component.ts
@@ -14,12 +14,20 @@ export class SportRecommendationsComponent implements OnInit {
   suggestedSports: any[] = [];
 
   iconMap: { [key: string]: string } = {
-    'Soccer': 'https://www.reshot.com/preview-assets/icons/5Y3TB47EMK/football-player-5Y3TB47EMK.svg',
-    'Boxing': 'https://www.svgrepo.com/show/171268/soccer-player-running-behind-the-ball.svg',
-    'Yoga': 'https://www.reshot.com/preview-assets/icons/9NGDPVTH4B/yoga-9NGDPVTH4B.svg',
-    'Skiing': 'https://www.svgrepo.com/show/171268/soccer-player-running-behind-the-ball.svg',
-    'Table Tennis': 'https://www.reshot.com/preview-assets/icons/T42E8JLPBU/tennis-player-T42E8JLPBU.svg',
-    'Default': ''
+    'Running': 'directions_run',
+    'Tennis': 'sports_tennis',
+    'Cycling': 'pedal_bike',
+    'Surfing': 'surfing',
+    'Basketball': 'sports_basketball',
+    'Yoga': 'self_improvement',
+    'Boxing': 'sports_mma',
+    'Climbing': 'terrain',
+    'Martial Arts': 'sports_kabaddi',
+    'Skiing': 'downhill_skiing',
+    'Table Tennis': 'sports_tennis',
+    'Volleyball': 'sports_volleyball',
+    'Riding': 'sports_horse_riding',
+    'Default': 'help_outline',
   };
 
   weatherData: any;
