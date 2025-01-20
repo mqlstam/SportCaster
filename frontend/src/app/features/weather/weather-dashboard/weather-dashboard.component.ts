5dc7512f13adcac3bf90279c1ca3c6b9d648ec28
not101Xander <xander380@gmail.com>
2025-01-20 02:16:47 +0100

    fixed merge issues

--- frontend/src/app/features/weather/weather-dashboard/weather-dashboard.component.ts
+++ frontend/src/app/features/weather/weather-dashboard/weather-dashboard.component.ts
@@ -24,7 +24,7 @@ export class WeatherDashboardComponent implements OnInit {
   errorMessage: string | null = null;
   manualLat: number = 0;
   manualLon: number = 0;
-  suggestions: string[] = [];
+
 
   constructor(
     private weatherService: WeatherService,
@@ -212,26 +212,9 @@ export class WeatherDashboardComponent implements OnInit {
     );
   }
 
-  onCityInputChange(): void {
-    if (this.city.trim().length > 2) { // Start met zoeken vanaf 3 karakters
-      this.weatherService.getCitySuggestions(this.city).subscribe(
-        (data: string[]) => {
-          this.suggestions = data;
-        },
-        (error) => {
-          console.error('Error fetching city suggestions:', error);
-        }
-      );
-    } else {
-      this.suggestions = []; // Wis suggesties als de invoer te kort is
-    }
-  }
 
-  selectSuggestion(suggestion: string): void {
-    this.city = suggestion;
-    this.suggestions = []; // Wis de suggesties na selectie
-    console.log('Geselecteerde stad:', suggestion);
-  }
+
+
 
   // onSubmit(): void {
   //   console.log('Manual input:', this.manualLat, this.manualLon);
